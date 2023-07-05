422d6e30d1c48b0c7d0ca4ac3fd037a963ffca2e
MESmirnov69 <M.E.Smirnov@yandex.ru>
2023-07-06 01:44:45 +0300

    add files and styles

--- src/index.js
+++ src/index.js
@@ -21,3 +21,27 @@ var swiper = new Swiper(".offerSwiper", {
   },
   modules: [Navigation, Pagination],
 });
+var swiper = new Swiper(".trendsSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  modules: [Navigation, Pagination],
+});
+var swiper = new Swiper(".categorySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
